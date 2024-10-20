bd2841a4dacf2d7b72aad8cf1b3d16ed989c704e
HuaXin <1929523645@qq.com>
2024-10-20 17:32:31 +0800

    增加宠物相关后台接口

--- petPlatform/models/pet.js
+++ petPlatform/models/pet.js
@@ -10,28 +10,39 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // 与 PetPhoto 模型关联，一个 Pet 可以有多个 PetPhoto
       Pet.hasMany(models.PetPhoto, {
-        foreignKey: 'pet_id',
-        onDelete: 'CASCADE', // 当 Pet 被删除时，相关的照片也删除
+        foreignKey: "pet_id",
+        onDelete: "CASCADE", // 当 Pet 被删除时，相关的照片也删除
       });
 
       //关联用户表等
       Pet.belongsTo(models.User, {
-        foreignKey: 'user_id',
-        onDelete: 'CASCADE', // 当用户被删除时，相应的宠物记录也删除
+        foreignKey: "user_id",
+        onDelete: "CASCADE", // 当用户被删除时，相应的宠物记录也删除
       });
-    
+
       Pet.belongsTo(models.User, {
-        foreignKey: 'adopter_id',
-        onDelete: 'SET NULL', // 当领养者被删除时，宠物的 adopter_id 设为 null
+        foreignKey: "adopter_id",
+        onDelete: "SET NULL", // 当领养者被删除时，宠物的 adopter_id 设为 null
       });
     }
   }
   Pet.init(
     {
       // ID
-      user_id: DataTypes.INTEGER,
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
       // 名
-      name: DataTypes.STRING,
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [1, 100], // 长度在5到10之间
+        },
+      },
       // 年龄
       age: DataTypes.INTEGER,
       // 性别
@@ -62,11 +73,11 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "Pet",
       indexes: [
         {
-          fields: ['user_id'],
+          fields: ["user_id"],
         },
         {
-          fields: ['adopter_id'],
-        }
+          fields: ["adopter_id"],
+        },
       ],
     }
   );
