9f0f73b50480dc98614e233cc04ccc0d72942a0d
HuaXin <1929523645@qq.com>
2024-10-22 18:36:43 +0800

    增加修改用户信息

---
+++ petPlatform_vue/src/views/Home/userCenter.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="user-center">
+        <div class="user-info">
+            <div class="user-info-item">
+                <div class="user-info-name">账号</div>
+                <el-input v-model="userInfo.id" disabled placeholder="Please input" />
+            </div>
+            <div class="user-info-item">
+                <div class="user-info-name">用户名</div>
+                <el-input v-model="userInfo.name" placeholder="Please input" />
+            </div>
+            <div class="user-info-item">
+                <el-radio-group v-model="userInfo.gender">
+                    <el-radio value="female">女生</el-radio>
+                    <el-radio value="male">男生</el-radio>
+                    <el-radio value="other">其他</el-radio>
+                </el-radio-group>
+            </div>
+            <div class="user-info-item">
+                <div class="user-info-name">联系方式</div>
+                <el-input v-model="userInfo.contact_info" placeholder="Please input" />
+            </div>
+            <div class="user-info-item">
+                <el-popconfirm @confirm="confirm" title="确认修改信息吗?" confirm-button-text="确认" cancel-button-text="取消">
+                    <template #reference>
+                        <el-button type="primary">更新</el-button>
+                    </template>
+                </el-popconfirm>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { useUserStore } from "@/stores/userStore";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const userStore = useUserStore();
+const userInfo = ref(userStore.userInfo);
+async function confirm() {
+    const obj = {
+        name: userInfo.value.name,
+        gender: userInfo.value.gender,
+        contact_info: userInfo.value.contact_info
+    }
+    const flag = await userStore.updateUserMessage(userInfo.value.id, obj)
+    if (flag) {
+        ElMessage.success('修改信息成功！')
+    } else {
+        ElMessage.error('修改信息失败！')
+    }
+}
+</script>
+<style scoped lang="less">
+.user-center {
+    width: 100%;
+    margin-top: 20px;
+}
+
+.user-info {
+    text-align: center;
+    background-color: #f1f1f3;
+    padding: 0px;
+    margin: 0px;
+    height: 650px;
+    border-radius: 15px;
+
+    &-item {
+        display: flex;
+        height: 30px;
+        justify-content: center;
+        margin-bottom: 20px
+    }
+
+    &-name {
+        margin-right: 20px;
+        width: 200px;
+        line-height: 30px;
+    }
+}
+</style>
--- petPlatform_vue/src/views/Layout/home.vue
+++ petPlatform_vue/src/views/Layout/home.vue
@@ -1,11 +1,15 @@
 <script setup>
 import homeAside from './homeAside.vue';
+import Nav from './Nav.vue';
 
 </script>
 <template>
     <div class="layout">
         <homeAside />
-        <RouterView />
+        <div class="main">
+            <Nav />
+            <RouterView />
+        </div>
     </div>
 </template>
 
@@ -14,5 +18,11 @@ import homeAside from './homeAside.vue';
     display: flex;
     height: 100vh;
     width: 100vw;
+}
+.main {
+    flex: 1;
+    overflow: auto;
+    padding-left:2px;
+    box-sizing: border-box;
 }
 </style>
