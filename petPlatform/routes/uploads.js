37fb4e01e3e3cbfee45f06d5c28faabcc6e62a7f
HuaXin <1929523645@qq.com>
2024-10-26 15:56:45 +0800

    实现阿里云批量照片上传，同时更新对应id的数据库数据

--- petPlatform/routes/uploads.js
+++ petPlatform/routes/uploads.js
@@ -2,31 +2,85 @@ var express = require("express");
 var router = express.Router();
 const { BadRequest } = require("http-errors");
 const { NotFoundError, success, failure } = require("../utils/response");
-const { config, client, singleFileUpload } = require("../utils/aliyun");
+const { config, client, multipleFilesUpload } = require("../utils/aliyunArray");
 const { PetPhoto } = require("../models");
 
 /**
  * 阿里云oss客户端上传
  */
-router.post("/aliyun", function (req, res, next) {
+// router.post("/aliyun", function (req, res, next) {
+//   try {
+//     singleFileUpload(req, res, async function (error) {
+//       if (error) {
+//         return failure(res, error);
+//       }
+
+//       if (!req.file) {
+//         return failure(res, new BadRequest("请上传文件"));
+//       }
+//       //   console.log("Pet ID:", req.body.pet_id);
+//       //   记录附件信息
+//       await PetPhoto.create({
+//         ...req.file,
+//         pet_id: req.body.pet_id,
+//         fullpath: req.file.path + "/" + req.file.filename,
+//       });
+
+//       success(res, "上传成功", { file: req.file });
+//     });
+//   } catch (err) {
+//     failure(res, err);
+//   }
+// });
+
+
+/**
+ * 阿里云OSS客户端批量上传
+ */
+router.post("/aliyun/:pet_id", async function (req, res) {
   try {
-    singleFileUpload(req, res, async function (error) {
+    const { pet_id } = req.params;
+
+    // 检查 pet_id 是否存在
+    if (!pet_id) {
+      return failure(res, new BadRequest("pet_id 不能为空"));
+    }
+
+    // 查找当前宠物的所有照片记录
+    const oldPhotos = await PetPhoto.findAll({ where: { pet_id } });
+
+    // 如果存在旧照片，先删除它们
+    if (oldPhotos.length > 0) {
+      // 从旧照片记录中获取阿里云OSS上文件的路径
+      const pathsToDelete = oldPhotos.map((photo) => photo.fullpath);
+
+      // 删除阿里云OSS上的旧照片
+      await client.deleteMulti(pathsToDelete, { quiet: true });
+
+      // 删除数据库中的旧照片记录
+      await PetPhoto.destroy({ where: { pet_id } });
+    }
+
+    // 上传新照片
+    multipleFilesUpload(req, res, async function (error) {
       if (error) {
         return failure(res, error);
       }
 
-      if (!req.file) {
+      if (!req.files || req.files.length === 0) {
         return failure(res, new BadRequest("请上传文件"));
       }
-      //   console.log("Pet ID:", req.body.pet_id);
-      //   记录附件信息
-      await PetPhoto.create({
-        ...req.file,
-        pet_id: req.body.pet_id,
-        fullpath: req.file.path + "/" + req.file.filename,
-      });
 
-      success(res, "上传成功", { file: req.file });
+      // 批量插入新照片记录
+      const photoRecords = req.files.map((file) => ({
+        pet_id,
+        fullpath: file.path + "/" + file.filename,
+        ...file,
+      }));
+
+      await PetPhoto.bulkCreate(photoRecords);
+
+      success(res, "照片更新成功", { files: req.files.url });
     });
   } catch (err) {
     failure(res, err);
---
+++ petPlatform/utils/aliyunArray.js
@@ -0,0 +1,45 @@
+const multer = require("multer");
+const MAO = require("multer-aliyun-oss");
+const OSS = require("ali-oss");
+const { BadRequest } = require("http-errors");
+
+// 阿里云配置信息
+const config = {
+  region: process.env.ALIYUN_REGION,
+  accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID,
+  accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET,
+  bucket: process.env.ALIYUN_BUCKET,
+};
+
+// 阿里云上传
+const client = new OSS(config);
+
+// multer配置
+const upload = multer({
+  storage: MAO({
+    config: config,
+    destination: "uploadsArray", // 自定义上传目录
+  }),
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 限制文件大小为5MB
+  },
+  fileFilter: function (req, file, cb) {
+    // 限制文件类型--只允许上传图片
+    const fileType = file.mimetype.split("/")[0];
+    const isImage = fileType === "image";
+
+    if (!isImage) {
+      return cb(new BadRequest("只允许上传图片"));
+    }
+    cb(null, true);
+  },
+});
+
+// 上传多个文件
+const multipleFilesUpload = upload.array("files", 10); // 这里设置最多上传10个文件
+
+module.exports = {
+  config,
+  client,
+  multipleFilesUpload,
+};
